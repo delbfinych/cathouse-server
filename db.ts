841b54e2c6af70b52829c788e852bfe1fc666d29
delbfinych <dmi.villione@gmail.com>
2021-06-04 17:42:54 +0300

    u

--- db.ts
+++ db.ts
@@ -1,13 +1,12 @@
 import { Sequelize } from 'sequelize';
 import { config } from 'dotenv';
 config();
-console.log(process.env.DATABASE_URL);
+console.log();
 
 export default new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
         ssl: true,
-        rejectUnauthorized: true,
     },
 });
