c00b4c0c3f9e5dd8b1cdf723335db05b9c3a2328
delbfinych <dmi.villione@gmail.com>
2021-06-11 14:27:35 +0300

    u

--- routes/media.ts
+++ routes/media.ts
@@ -2,14 +2,14 @@ import { Router } from 'express';
 import authController from '../controllers/auth.controller';
 import mediaController from '../controllers/media.controller';
 import { upload } from '../multer';
+import fetch from 'node-fetch';
 const router = Router();
 
 router
-    .get('/:path', (req, res) => {
-        res.redirect(
-            'https://raw.githubusercontent.com/delbfinych/heroku-files/master' +
-                req.url
-        );
+    .get('/:path', async (req, res) => {
+        const response = await fetch(process.env.IMAGE_REMOTE_URL + req.url);
+        res.set({ 'content-type': response.headers.get('content-type') });
+        res.send(await response.buffer());
     })
     .post(
         '/',
